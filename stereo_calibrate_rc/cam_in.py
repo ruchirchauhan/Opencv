c92b225b1e609a8ddab2bd0696fd61e086dda1dd
ruchirchauhan <ruchir.chauhan@novisecurity.com>
2016-07-08 06:24:24 -0600

    stereo calibration code

--- stereo_calibrate_rc/cam_in.py
+++ stereo_calibrate_rc/cam_in.py
@@ -1,1 +1,61 @@
-/home/rc/MyPrograms/opencv_workspace/stereo_calibrate_rc/cam_in.py
+#!/usr/bin/python
+import numpy as np
+import cv2
+import sys
+
+args = sys.argv
+del args[0]
+
+def capture(cam_l, cam_r):
+    cap_l = cv2.VideoCapture(cam_l)
+    cap_r = cv2.VideoCapture(cam_r)
+    debug_dir = 'data/'
+    capture_counter = 0
+
+    while(True):
+
+        #Capture frame-by-frame
+        ret1, frame_l = cap_l.read()
+        ret2, frame_r = cap_r.read()
+
+        #Operations on the frame
+        out_frame_l = cv2.cvtColor(frame_l, cv2.COLOR_BGR2GRAY)
+        out_frame_r = cv2.cvtColor(frame_r, cv2.COLOR_BGR2GRAY)
+
+        #Display the resulting frame
+        cv2.imshow('Left camera', out_frame_l)
+        cv2.imshow('Right camera', out_frame_r)
+
+        key_press = cv2.waitKey(1)
+        if key_press & 0xFF == ord('q'):
+            break
+        if key_press & 0xFF == ord('c'):
+            capture_counter += 1
+            print 'capture: ', capture_counter
+            linfile = debug_dir + 'left0' + str(capture_counter) + '.jpg'
+            rinfile = debug_dir + 'right0' + str(capture_counter) + '.jpg'
+            cv2.imwrite(linfile, out_frame_l)
+            cv2.imwrite(rinfile, out_frame_r)
+            if capture_counter == 20:
+                print '20 images captures. Enough for calibration, press q to stop taking images'
+
+    #When everything done, release the capture
+    cap_l.release()
+    cap_r.release()
+    cv2.destroyAllWindows()
+
+###############
+# Main
+###############
+
+# Take user input
+if not args:
+    print 'Please select a camera'
+    exit(1)
+cam_l = args[0]
+cam_r = args[1]
+
+#print 'User selected camera no: ', cam_no
+capture(int(cam_l), int(cam_r))
+
+exit(0)
--- stereo_calibrate_rc/cam_in_stereo_calib.sh
+++ stereo_calibrate_rc/cam_in_stereo_calib.sh
@@ -1,1 +1,79 @@
-/home/rc/MyPrograms/opencv_workspace/stereo_calibrate_rc/cam_in_stereo_calib.sh
+#!/usr/bin/env bash
+
+c_lred=91
+c_lgreen=92
+c_dflt=0
+data_folder=data/
+
+cleanup() {
+    exit 0
+}
+trap cleanup 0 1 2 3 15
+
+take_images() {
+    local cam_left="$1"
+    local cam_right="$2"
+    python cam_in.py $cam_left $cam_right
+    return $?
+}
+
+images_available_for_calib() {
+    total_images=$(ls -l ${data_folder} |wc -l)
+    return ${total_images}
+}
+calib_cam() {
+    ./stereo_calib
+    return $?
+}
+
+print_help() {
+  echo '************************************************************'
+  echo 'Usage: '
+  echo './cam_in_stereo_calib.sh <Index:cam_left> <Index:cam_right>'
+  echo '************************************************************'
+}
+
+########
+# Main #
+########
+
+# Take indexes of both stereo cameras as user input
+cam_left="$1"
+cam_right="$2"
+
+# Return error if user input not detected
+if [ "$cam_left" == "" ]; then
+  print_help
+  exit 1
+fi
+if [ "$cam_right" == "" ]; then
+  print_help
+  exit 1
+fi
+#
+# Capture images from both the stereo cameras
+#
+take_images $cam_left $cam_right
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo -e "\e[${c_lred}mTaking pictures failed\e[${c_dflt}m"
+    exit 1
+fi
+
+images_available_for_calib
+total_images=$?
+if [ $total_images -lt 40 ]; then
+    echo -e "\e[${c_lred}mNot enough images for calibration \e[${c_dflt}m"
+    exit 1
+fi
+
+calib_cam
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo -e "\e[${c_lred}mStereo calibration failed\e[${c_dflt}m"
+    exit 1
+fi
+
+echo -e "\e[${c_lgreen}mStereo calibration  successful\e[${c_dflt}m"
+
+exit 0
